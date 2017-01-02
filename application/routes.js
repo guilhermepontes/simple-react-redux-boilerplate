f6436ad979ebb61ed8e1cb837c71d91876bc5f51
Guilherme Pontes <guilhermepontes@msn.com>
2017-01-02 18:00:36 +0100

    Add routes.

---
+++ application/routes.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, IndexRoute } from 'react-router'
+
+import Root from './containers/Root'
+import HomePage from './containers/HomePage'
+import AboutPage from './containers/AboutPage'
+import NotFoundPage from './containers/NotFoundPage'
+
+const routes = (
+  <Route path="/" component={Root}>
+    <IndexRoute component={HomePage}/>
+    <Route path="/about" component={AboutPage} />
+
+    <Route path="*" component={NotFoundPage} />
+  </Route>
+)
+
+export default routes
