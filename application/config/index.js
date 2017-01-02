1037b035b03dd06e8ff76bf65fec204478db8d21
Guilherme Pontes <guilhermepontes@msn.com>
2017-01-02 17:59:43 +0100

    Add config file.

---
+++ application/config/index.js
@@ -0,0 +1,9 @@
+const API_URL = 'https://api.themoviedb.org/3'
+const API_KEY = '96e769aaf4e0ffe7112b1cc2cbdd9db9'
+const IMAGE_ASSET_BASE = 'http://image.tmdb.org/t/p/w185'
+
+export {
+  API_URL,
+  API_KEY,
+  IMAGE_ASSET_BASE
+}
