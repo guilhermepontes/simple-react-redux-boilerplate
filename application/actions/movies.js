2ec7eaa89e90108c2de46f83e3fb23cfa28b6065
Guilherme Pontes <guilhermepontes@msn.com>
2017-08-30 14:38:01 +0200

    Update movies.js

--- application/actions/movies.js
+++ application/actions/movies.js
@@ -3,10 +3,7 @@ import { API_URL, API_KEY } from '../config'
 import types from '../constants/actionTypes'
 
 const fetchMoviesByGenre = (genreId) => {
-  const apiUrl = API_URL
-  const apiKey = API_KEY
-
-  let api_endpoint = `${apiUrl}/genre/${genreId}/movies?api_key=${apiKey}&language=en-US`
+  const api_endpoint = `${API_URL}/genre/${genreId}/movies?api_key=${API_KEY}&language=en-US`
 
   return {
     type: types.FETCH_MOVIES,
